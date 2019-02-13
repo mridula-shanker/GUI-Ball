972a5223837d9d58b45be5e7669b11b1f720183b
mshanker22 <mshanker22@HS-MATH2-06.gvsd.org>
2019-02-13 13:34:32 -0500

    2/13/2019

---
+++ GUI-Ball/src/Ball.java
@@ -0,0 +1,25 @@
+/**
+ * Mridula Shanker
+ * Period 6
+ */
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JComponent;
+
+	public class Ball extends JComponent
+		{
+			private Ellipse2D.Double ball;
+			
+			public Ball()
+			{
+					ball=new Ellipse2D.Double(20,20,10,10);
+			}
+			
+			public void paintComponent(Graphics g) //this method already exists, we're overriding it
+			{
+				Graphics2D g2 = (Graphics2D)g;
+				g2.draw(ball);
+			}
+		}
+
